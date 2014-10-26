e956333f0f30720aadf725569385531ea0868049
Juin Chiu <davidjuin0519@gmail.com>
2014-10-26 19:58:31 +0800

    add cols to table meetup & event

--- db/migrate/20141026103201_create_meetups.rb
+++ db/migrate/20141026103201_create_meetups.rb
@@ -1,7 +1,14 @@
 class CreateMeetups < ActiveRecord::Migration
   def change
     create_table :meetups do |t|
-
+      t.integer :category_id
+      t.string :title
+      t.string :subtitle
+      t.text :description
+      t.integer :day
+      t.string :location
+      t.string :called
+      
       t.timestamps
     end
   end
--- db/migrate/20141026103419_create_events.rb
+++ db/migrate/20141026103419_create_events.rb
@@ -1,7 +1,13 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-
+      t.integer :meetup_id
+      t.string :subject
+      t.string :content
+      t.datetime :date
+      t.string :place
+      t.integer :price
+      t.integer :score
       t.timestamps
     end
   end
