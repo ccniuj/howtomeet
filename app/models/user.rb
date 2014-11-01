1e8ab1fcd2fd6e04e54283134aa743299216f453
Juin Chiu <davidjuin0519@gmail.com>
2014-11-02 03:23:36 +0800

    add :omniauthable to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:google_oauth2]
 
   has_many :meetup_members
   has_many :meetups, as: :meetable, through: :meetup_members, dependent: :destroy
